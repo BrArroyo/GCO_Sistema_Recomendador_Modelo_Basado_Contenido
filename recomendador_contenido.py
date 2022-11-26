3595db53b9b66eb320941d2d7e5081d578d7effd
bruno <bruno.lorenzo.arroyo@gmail.com>
2022-11-26 16:33:10 +0000

    ver 0.1

---
+++ recomendador_contenido.py
@@ -0,0 +1,12 @@
+import argparse
+import collections
+import math
+import sys
+
+# ==============================================================================
+#                           EJECUCION DE LAS FUNCIONES
+# ==============================================================================
+
+
+if __name__ == '__main__':
+  print ('Sistema Recomendador Basado en el Contenido')
